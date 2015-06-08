46cd3a195adad28c857283310aa9d4d8f6e80dd3
jgabrielfreitas <jgabrielafreitas@gmail.com>
2015-06-07 21:44:44 -0300

    Java desktop translator v1.1

---
+++ JavaFxTranslatorApplication/src/exceptions/ExtensionNotSupportedException.java
@@ -0,0 +1,13 @@
+package exceptions;
+
+public class ExtensionNotSupportedException extends Exception {
+	
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ExtensionNotSupportedException() {
+	}
+
+}
